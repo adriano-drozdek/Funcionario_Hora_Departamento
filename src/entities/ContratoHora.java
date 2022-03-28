23dde05afab0de271a7c272ada75d96135a39da3
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-03-28 14:29:01 -0300

    Criando entidade CONTRATO POR HORA.

---
+++ src/entities/ContratoHora.java
@@ -0,0 +1,45 @@
+package entities;
+
+import java.util.Date;
+
+public class ContratoHora {
+
+	private Date data;
+	private Double ValorHora;
+	private Integer hora;
+
+	public ContratoHora(Date data, Double valorHora, Integer hora) {
+		this.data = data;
+		ValorHora = valorHora;
+		this.hora = hora;
+	}
+
+	public ContratoHora() {
+
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Double getValorHora() {
+		return ValorHora;
+	}
+
+	public void setValorHora(Double valorHora) {
+		ValorHora = valorHora;
+	}
+
+	public Integer getHora() {
+		return hora;
+	}
+
+	public void setHora(Integer hora) {
+		this.hora = hora;
+	}
+
+}
