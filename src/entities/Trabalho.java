1d905df99d58392d54c5110e95d2e53d801b9520
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-03-29 10:21:58 -0300

    Adicionando getters and setters/constructor using fields e INSTANCIANDO a lista de contratos.

--- src/entities/Trabalho.java
+++ src/entities/Trabalho.java
@@ -1,5 +1,6 @@
 package entities;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import entities.enuns.NivelDeTrabalho;
@@ -11,6 +12,57 @@ public class Trabalho {
 	private Double SalarioBase;
 
 	private Departamento departamernto;
-	private List<ContratoHora> contratos;
+	private List<ContratoHora> contratos = new ArrayList<>();
+
+	public Trabalho(String nome, NivelDeTrabalho nivel, Double salarioBase, Departamento departamernto) {
+		this.nome = nome;
+		this.nivel = nivel;
+		SalarioBase = salarioBase;
+		this.departamernto = departamernto;
+	}
+
+	public Trabalho() {
+
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public NivelDeTrabalho getNivel() {
+		return nivel;
+	}
+
+	public void setNivel(NivelDeTrabalho nivel) {
+		this.nivel = nivel;
+	}
+
+	public Double getSalarioBase() {
+		return SalarioBase;
+	}
+
+	public void setSalarioBase(Double salarioBase) {
+		SalarioBase = salarioBase;
+	}
+
+	public Departamento getDepartamernto() {
+		return departamernto;
+	}
+
+	public void setDepartamernto(Departamento departamernto) {
+		this.departamernto = departamernto;
+	}
+
+	public List<ContratoHora> getContratos() {
+		return contratos;
+	}
+
+	public void setContratos(List<ContratoHora> contratos) {
+		this.contratos = contratos;
+	}
 
 }
